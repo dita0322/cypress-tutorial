20247f1899ddac8de864b31b21846ba77f5e37d1
Daniel Ramirez Gallo <daniel.ramirez@grupokonecta.com>
2023-03-31 14:20:47 -0500

    end 02

--- 02 Starting Project/cypress/e2e/tasks.cy.js
+++ 02 Starting Project/cypress/e2e/tasks.cy.js
@@ -38,4 +38,54 @@ describe("Task Management", () => {
     cy.get("dialog").should("not.exist");
     cy.get(".backdrop").should("not.exist");
   });
+
+  it("shoould validate user input", () => {
+    cy.visit("http://localhost:5173");
+    cy.get("button", { name: "New Task" }).click();
+    cy.get("dialog").contains("Add Task").click();
+
+    // NOTA: Se puede hacer match con parte de texto
+    cy.contains("Please provide values").should("be.visible");
+  });
+
+  it("should filter tasks", () => {
+    const title = "My new task";
+    const summary = "Some description";
+
+    cy.visit("http://localhost:5173");
+    cy.get("button", { name: "New Task" }).click();
+    cy.get("#title").type(title);
+    cy.get("#summary").type(summary);
+    // .select() Para seleccionar un elemento de una lista
+    cy.get("#category").select("urgent");
+    cy.get("dialog").contains("Add Task").click();
+    cy.get(".task").should("have.length", 1);
+    cy.get("#filter").select("moderate");
+    cy.get(".task").should("have.length", 0);
+    cy.contains("No tasks found!").should("be.visible");
+    cy.get("#filter").select("urgent");
+    cy.get(".task").should("have.length", 1);
+    cy.get("#filter").select("all");
+    cy.get(".task").should("have.length", 1);
+  });
+
+  it("should add multiple tasks", () => {
+    cy.visit("http://localhost:5173");
+    cy.get("button", { name: "New Task" }).click();
+    cy.get("#title").type("Task 1");
+    cy.get("#summary").type("First task");
+    cy.get("dialog").contains("Add Task").click();
+    cy.get(".task").should("have.length", 1);
+
+    cy.get("button", { name: "New Task" }).click();
+    cy.get("#title").type("Task 2");
+    cy.get("#summary").type("Second task");
+    cy.get("dialog").contains("Add Task").click();
+    cy.get(".task").should("have.length", 2);
+
+    // cy.get('.task').eq(0) Lo mismo que el siguiente
+    cy.get(".task").first().contains("Task 1");
+    // cy.get('.task').eq(1) Lo mismo que el siguiente
+    cy.get(".task").last().contains("Task 2");
+  });
 });
