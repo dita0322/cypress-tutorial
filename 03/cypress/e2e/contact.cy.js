49901bf56d24d3703ead267ac6bacb313ff6b962
Daniel Ramirez Gallo <daniel.ramirez@grupokonecta.com>
2023-04-04 06:59:22 -0500

    start 04

--- 03/cypress/e2e/contact.cy.js
+++ 03/cypress/e2e/contact.cy.js
@@ -1,56 +1,72 @@
-describe("contact form", () => {
-  it("should submit the form", () => {
-    cy.visit("http://localhost:5173/about");
-    cy.get("[data-cy='contact-input-message']").type("Hello World");
-    cy.get("[data-cy='contact-input-name']").type("John Doe");
-    cy.get("[data-cy='contact-input-email']").type("test@example.com");
-    cy.get("[data-cy='contact-btn-submit']").as("submitBtn");
-    cy.get("@submitBtn")
-      .contains("Send Message")
-      .and("not.have.attr", "disabled");
-    cy.get("@submitBtn").click();
-    cy.get("@submitBtn").contains("Sending...").should("have.attr", "disabled");
-  });
+describe(
+  "contact form",
+  {
+    defaultCommandTimeout: 3000,
+  },
+  () => {
+    it(
+      "should submit the form",
+      {
+        defaultCommandTimeout: 5000,
+      },
+      () => {
+        cy.visit("/about");
+        cy.get("[data-cy='contact-input-message']").type("Hello World");
+        cy.get("[data-cy='contact-input-name']").type("John Doe");
+        cy.get("[data-cy='contact-input-email']").type("test@example.com");
+        cy.get("[data-cy='contact-btn-submit']").as("submitBtn");
+        cy.get("@submitBtn")
+          .contains("Send Message")
+          .and("not.have.attr", "disabled");
+        cy.get("@submitBtn").click();
+        cy.get("@submitBtn")
+          .contains("Sending...")
+          .should("have.attr", "disabled");
+      },
+    );
 
-  it("should submit the form with enter key", () => {
-    cy.visit("http://localhost:5173/about");
-    cy.get("[data-cy='contact-input-message']").type("Hello World");
-    cy.get("[data-cy='contact-input-name']").type("John Doe");
-    cy.get("[data-cy='contact-btn-submit']").as("submitBtn");
-    cy.get("@submitBtn")
-      .contains("Send Message")
-      .and("not.have.attr", "disabled");
+    it("should submit the form with enter key", () => {
+      cy.visit("/about");
+      cy.get("[data-cy='contact-input-message']").type("Hello World");
+      cy.get("[data-cy='contact-input-name']").type("John Doe");
+      cy.get("[data-cy='contact-btn-submit']").as("submitBtn");
+      cy.get("@submitBtn")
+        .contains("Send Message")
+        .and("not.have.attr", "disabled");
 
-    // Otros comandos: https://docs.cypress.io/api/commands/type
-    cy.get("[data-cy='contact-input-email']").type("test@example.com{enter}");
-    cy.get("@submitBtn").contains("Sending...").should("have.attr", "disabled");
-  });
+      // Otros comandos: https://docs.cypress.io/api/commands/type
+      cy.get("[data-cy='contact-input-email']").type("test@example.com{enter}");
+      cy.get("@submitBtn")
+        .contains("Sending...")
+        .should("have.attr", "disabled");
+    });
 
-  it("should validate the form input", () => {
-    cy.visit("http://localhost:5173/about");
-    cy.get("[data-cy='contact-btn-submit']").as("submitBtn");
-    cy.get("[data-cy='contact-input-message']").as("inputMessage");
-    cy.get("[data-cy='contact-input-name']").as("inputName");
-    cy.get("[data-cy='contact-input-email']").as("inputEmail");
-    cy.get("@submitBtn").click();
-    cy.get("@submitBtn")
-      .should("not.contain", "Sending...")
-      .should("not.have.attr", "disabled");
-    cy.get("@submitBtn").contains("Send Message");
-    cy.get("@inputMessage").focus().blur();
-    cy.get("@inputMessage")
-      .parent()
-      .should("have.attr", "class")
-      .and("match", /invalid/);
-    cy.get("@inputName").focus().blur();
-    cy.get("@inputName")
-      .parent()
-      .should("have.attr", "class")
-      .and("contains", "invalid");
-    cy.get("@inputEmail").focus().blur();
-    cy.get("@inputEmail")
-      .parent()
-      .should("have.attr", "class")
-      .and("contains", "invalid");
-  });
-});
+    it("should validate the form input", () => {
+      cy.visit("/about");
+      cy.get("[data-cy='contact-btn-submit']").as("submitBtn");
+      cy.get("[data-cy='contact-input-message']").as("inputMessage");
+      cy.get("[data-cy='contact-input-name']").as("inputName");
+      cy.get("[data-cy='contact-input-email']").as("inputEmail");
+      cy.get("@submitBtn").click();
+      cy.get("@submitBtn")
+        .should("not.contain", "Sending...")
+        .should("not.have.attr", "disabled");
+      cy.get("@submitBtn").contains("Send Message");
+      cy.get("@inputMessage").focus().blur();
+      cy.get("@inputMessage")
+        .parent()
+        .should("have.attr", "class")
+        .and("match", /invalid/);
+      cy.get("@inputName").focus().blur();
+      cy.get("@inputName")
+        .parent()
+        .should("have.attr", "class")
+        .and("contains", "invalid");
+      cy.get("@inputEmail").focus().blur();
+      cy.get("@inputEmail")
+        .parent()
+        .should("have.attr", "class")
+        .and("contains", "invalid");
+    });
+  },
+);
